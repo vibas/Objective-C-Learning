cc948ca854691c4df626ce4697d2fcf73c7ff7c4
vibasjet2022 <vibas.behera@jetsynthesys.com>
2023-03-10 18:52:58 +0530

    Function added in main

--- FirstMacOS_CMD_App/main.m
+++ FirstMacOS_CMD_App/main.m
@@ -7,37 +7,67 @@
 
 #import <Foundation/Foundation.h>
 #import "MyClasses.h"
-#pragma mark
+
+void HelloWorld(void);
+void VariableTest(void);
+void CalculatorTest(void);
+void TestLoop(void);
 
 int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
-        // HELLO WORLD
-        {
-            //        NSString *s = @"Hello world!";
-            //        NSLog(@"%@", s);
-        }
-        
-        // CALCULATOR
-        {
-            //        Calculator *calc = [[Calculator alloc] init];
-            //        [calc Test];
-        }
-        
-        // LOOP TEST
-        {
-            LoopTest *loopTest = [[LoopTest alloc]init];
-//            [loopTest ForLoop];
-//            [loopTest WhileLoop];
-//            [loopTest DoWhileLoop];
-            [loopTest NestedLoop];
-        }
-        
-        
-        
+        HelloWorld();
+        VariableTest();
+        CalculatorTest();
+        TestLoop();
     }
     return 0;
 }
 
+
+void HelloWorld()
+{
+    NSString *s = @"Hello world!";
+    NSLog(@"%@", s);
+}
+
+void VariableTest()
+{
+    // VARIABLE
+    {
+        int i = 5;
+        float f = 5.5f;
+        char c = 'V';
+        double d = 500000.5;
+        Byte b = 20;
+        
+        NSLog(@"int %d",i);
+        NSLog(@"float %f",f);
+        NSLog(@"char %c",c);
+        NSLog(@"double %f", d);
+        NSLog(@"byte %d",b);
+    }
+}
+
+void CalculatorTest()
+{
+    // CALCULATOR
+    {
+        Calculator *calc = [[Calculator alloc] init];
+        [calc Test];
+    }
+}
+
+void TestLoop()
+{
+    // LOOP TEST
+    {
+        LoopTest *loopTest = [[LoopTest alloc]init];
+        [loopTest ForLoop];
+        [loopTest WhileLoop];
+        [loopTest DoWhileLoop];
+        [loopTest NestedLoop];
+    }
+}
 
