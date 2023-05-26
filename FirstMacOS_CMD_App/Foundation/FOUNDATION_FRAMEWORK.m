5b5b7bb0bec6d72eaa07d77471ff17758781c474
Vibas Ranjan Behera <vibas.behera@ext-pocketgems.com>
2023-05-26 15:56:57 +0530

    Minor issue fix

--- FirstMacOS_CMD_App/Foundation/FOUNDATION_FRAMEWORK.m
+++ FirstMacOS_CMD_App/Foundation/FOUNDATION_FRAMEWORK.m
@@ -12,8 +12,89 @@
 
 -(void) TestDataStorage
 {
+//    [self TestNSArray];
+    [self TestNSDict];
+    
+    
+    
+    // -------- mutable NSDictionary ----------
+}
+
+-(void) TestNSArray
+{
+    //---------------- immutable NSArray ----------------
+    NSArray *nsArrayOfString = [[NSArray alloc]initWithObjects:@"String1",@"String2",@"String3",nil];
+    unsigned long nsArrayOfStringCount = (unsigned long) nsArrayOfString.count;
+    NSLog(@"count of nsArrayOfString = %lu",nsArrayOfStringCount);
+    for(int i=0; i< nsArrayOfStringCount; i++)
+    {
+        NSLog(@"Object at index [%d] is %@", i, [nsArrayOfString objectAtIndex:i]);
+    }
+    
+    //-----------------mutable NSArray ------------------
+    NSMutableArray *nsMutableArrayOfString = [[NSMutableArray alloc]init];
+    [nsMutableArrayOfString addObjectsFromArray:nsArrayOfString];
+    [nsMutableArrayOfString addObject:@"String4"];
+    [nsMutableArrayOfString addObject:@"String5"];
+    NSLog(@"Added Objects");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+    
+    [nsMutableArrayOfString removeAllObjects];
+    NSLog(@"Removed All Objects");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+    
+    [nsMutableArrayOfString addObject:@"Hello"];
+    [nsMutableArrayOfString addObject:@"World"];
+    [nsMutableArrayOfString addObject:@"!"];
+    NSLog(@"Added Objects");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+    
+    [nsMutableArrayOfString removeObjectAtIndex:2];
+    NSLog(@"Removed Object at index");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+    
+    [nsMutableArrayOfString exchangeObjectAtIndex:1 withObjectAtIndex:0];
+    NSLog(@"Exchanged objects with index");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+    
+    [nsMutableArrayOfString replaceObjectAtIndex:0 withObject:@"Hello!"];
+    NSLog(@"Replace objects at index with object");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+    
+    [nsMutableArrayOfString insertObject:@"World" atIndex:1];
+    NSLog(@"Insert objects at index");
+    [self PrintNSMutalbeArrayCountAndElements:nsMutableArrayOfString];
+}
+
+-(void) PrintNSMutalbeArrayCountAndElements :(NSMutableArray*) nsMutableArr
+{
+//    NSLog(@"%@",nsMutableArr);
+    unsigned long nsMutableArrCount = (unsigned long) nsMutableArr.count;
+    NSLog(@"Count of nsMutableArrayOfString = %lu", nsMutableArrCount);
+    for(int i=0; i< nsMutableArrCount; i++)
+    {
+        NSLog(@"Object at index [%d] is %@", i, [nsMutableArr objectAtIndex:i]);
+    }
+}
+
+-(void) TestNSDict
+{
+    // -------- immutable NSDictionary ----------
+    NSDictionary *nsDict = [[NSDictionary alloc] initWithObjectsAndKeys:@100,@"Key1",@200,@"Key2", nil];
+    NSLog(@"Dict Key Value Pairs %@",nsDict);
+    NSLog(@"Dict all keys %@",[nsDict allKeys]);
+    NSLog(@"Dict all values %@",[nsDict allValues]);
+    NSLog(@"Dict value for key \"Key1""= %@",[nsDict valueForKey:@"Key1"]);
+    
+    // -------- mutable NSDictionary ----------
+    NSMutableDictionary *nsMutDict = [[NSMutableDictionary alloc]init];
+    [nsMutDict setValue:@500 forKey:@"Key1"];
+    [nsMutDict setValue:@600 forKey:@"Key2"];
+    NSLog(@"Mutable Dict Key Value Pairs %@",nsMutDict);
+    
     
 }
+
 -(void) TestTextAndString
 {
     
@@ -41,7 +122,7 @@
     NSLog(@"================ FOUNDATION FRAMEWORK ===============");
     
     // Learn about NSArray and NSDictionary
-//    [self TestDataStorage];
+    [self TestDataStorage];
     
 //    [self TestTextAndString];
 //    [self TestDatesAndTime];
